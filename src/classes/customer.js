3edc290566479dfb3baefc9cdf6dbcecdb8224d6
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-25 17:33:14 -0600

    Add methods for user bookings

--- src/classes/customer.js
+++ src/classes/customer.js
@@ -11,19 +11,48 @@ class Customer {
     };
   }
 
-  getBookings(bookingRepo) {
-    return bookingRepo.bookingList.filter(booking => booking.data.userID === this.id
-    )
+
+  getUserBookings(bookingRepo) {
+    return bookingRepo.findBookingByUser(this.id)
+  }
+
+  getTodaysDate() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() +1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    return `${yyyy}/${mm}/${dd}`;
+  };
+
+  getCurrentBookings(bookingRepo) {
+    let current = []
+    this.getUserBookings(bookingRepo).forEach(booking => {
+      let bookingDate = booking.data.date;
+      if (bookingDate >= this.getTodaysDate()) {
+        current.push(booking)
+      }
+    })
+    return current
+  }
+
+  getPastBookings(bookingRepo) {
+    let past = []
+    this.getUserBookings(bookingRepo).forEach(booking => {
+      let bookingDate = booking.data.date;
+      if (bookingDate < this.getTodaysDate()) {
+        past.push(booking)
+      }
+    })
+    return past
   }
 
   getTotalSpent(bookingRepo, roomRepo) {
-    return this.getBookings(bookingRepo).reduce((total, booking) => {
+    return this.getUserBookings(bookingRepo).reduce((total, booking) => {
       let bookedRoom = roomRepo.roomList.find(room => room.data.number === booking.data.roomNumber)
       total += bookedRoom.data.costPerNight
       return total
     }, 0).toFixed(2)
   }
-
 };
 
 export default Customer;
